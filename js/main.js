8f42cb3c4cc7bbd4c284785f2fba1acae0e8e55f
Dakota Kronberger <kodykronberger@yahoo.com>
2016-05-12 15:16:13 -0500

    Added AJAX to switch pages/title on click.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
 // On doc ready
-$(document).ready(function(){
+$(document).ready(function () {
     // Test ajax
     loadPageAjax("_home");
 }); // End of on doc ready
 
-function loadPageAjax(nameOfPage) {
-    var _url = "../content/" + nameOfPage + ".html";
+
+// Loads a file from content/ using AJAX
+function loadPageAjax(nameOfPage, jumboTitle) {
+    var _url = "./content/" + nameOfPage + ".html";
     $.ajax({
         url: _url,
-        success: function( data ) {
-            $("#content").html( data );
+        success: function (data) {
+            changeContent(data);
+            changeJumboTitle(jumboTitle);
         }
     });
+}
+
+// Changes the content of the page.
+function changeContent(data) {
+    $("#content").hide();
+    $("#content").html(data);
+    $("#content").fadeIn();
+}
+
+function changeJumboTitle(name) {
+    $(".jumbo-title").html(name);
 }
