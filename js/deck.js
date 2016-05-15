acbe584615f8a2e0d5c02aa69bbe4146fc5a985c
Dakota Kronberger <kodykronberger@yahoo.com>
2016-05-15 18:20:43 -0500

    Added big details to my deck page.

--- js/deck.js
+++ js/deck.js
@@ -17,7 +17,7 @@ $("document").ready(function(){
                     getAllImages(function(data){
                         allImages = data;
                         populateDecks(JSON.parse(result));
-                    })
+                    });
                 });
             }
         });
@@ -34,10 +34,133 @@ function findCardByName (name) {
 
 function populateDecks (data) {
     var html = "";
-
     for (var deckName in data) {
-        html += "<h1>"+deckName+"</h1><hr>"
+        var cardsLength = 0;
+        var totalPower = 0;
+        var totalToughness = 0;
+        var numberOfCreatures = 0;
+        var numberOfInstants = 0;
+        var numberOfLands = 0;
+        var mainColors = {
+            "white": 0,
+            "blue": 0,
+            "red": 0,
+            "black": 0,
+            green: 0
+        };
+        for(var i = 0; i < data[deckName].length; i++) {
+            cardsLength += data[deckName][i].quantity;
+        }
+        html += '<div class="panel panel-default">' +
+        '<div class="panel-heading" role="tab" id="headingFor'+deckName+'">'+
+            '<h4 class="panel-title">'+
+                '<a role="button" data-toggle="collapse" data-parent="#decks" href="#collapseFor'+deckName+'" aria-expanded="false" aria-controls="collapseFor'+deckName+'">'+
+                    '<h4>Mono-Black</h4>'+
+                '</a>'+
+                
+            '</h4>'+
+            '<h5> | '+cardsLength+' cards</h5>'+
+        '</div>'+
+        '<p></p>'+
+        '<div class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFor'+deckName+'" id="collapseFor'+deckName+'">'+
+           ' <div class="panel-body">' + 
+                '<ul class="list-group col-md-6">';
+        
+        // For each card...
+        for(var i = 0; i < data[deckName].length; i++) {
+            var card = findCardByName(data[deckName][i].cardName);
+
+            html += ' <li class="list-group-item card-group">'+
+                       ' <img class="cardthumb" width="30px" src="'+allImages[card.name]+'">'+
+                        '<span class="badge">'+data[deckName][i].quantity+'x</span>' +
+                        '<div class="cardInfo">' +
+                        '<span>'+data[deckName][i].cardName+'</span>'+
+                        '<ul class="manaicons">';
+            
+            var manaCost = card.manaCost;
+            if (manaCost) {
+                var manas = manaCost.replace(/}/g, "").replace(/{/g, "").split("");
+                console.log(manas);
+                for(var index in manas) {
+                    if(!isNaN(manas[index])){
+                        console.log(manas[index]);
+                        html += '<li><span class="badge badge-success">'+manas[index]+'</span></li>'
+                    } else {
+                        html += '<li><img width="20px" src="./img/';
+                            switch(manas[index].toLowerCase()) {
+                                case "r":
+                                    html += "redMana.png";
+                                    mainColors.red++;
+                                    break;
+                                case "w":
+                                    html += "whiteMana.png";
+                                    mainColors.white++;
+                                    break;
+                                case "u":
+                                    html += "blueMana.png";
+                                    mainColors.blue++;
+                                    break;
+                                case "g":
+                                    html += "greenMana.png";
+                                    mainColors.green++;
+                                    break;
+                                case "b":
+                                    html += "blackMana.png";
+                                    mainColors.black++;
+                                    break;
+                                default:
+                                    alert();
+                                    break;
+                            }
+                        html += '"</li>'
+                    }
+                }
+            }
+            
+            if (typeof card.power !== 'undefined' && typeof card.toughness !== 'undefined') {
+                html += '<span class="card-stats">( '+card.power+'/'+card.toughness+' )</span>';
+                totalPower += Number(card.power);
+                totalToughness += Number(card.toughness);
+            }
+                            
+            html +='</ul></div></li>';
+        }
+
+            html += '</ul>' +
+                '<ul class="list-group col-md-6">'+
+               ' <li class="list-group-item"><span class="list-title">Total Power: </span>'+totalPower+'</li>'+
+               ' <li class="list-group-item"><span class="list-title">Total Toughness: </span>'+totalToughness+'</li>'+
+                '<li class="list-group-item"><span class="list-title">Creatures: </span>'+numberOfCreatures+'</li>'+
+               ' <li class="list-group-item"><span class="list-title">Lands: </span>'+numberOfLands+'</li>'+
+               ' <li class="list-group-item"><span class="list-title">Instants or Spells: </span>'+numberOfInstants+'</li>'+
+               ' <li class="list-group-item">'+
+                   ' <em>Colors: </em>';
+
+            if(mainColors.black > 0) {
+                html += ' <img class="color-image" width="20px" src="./img/blackMana.png">';
+            }
+            if(mainColors.blue > 0) {
+                html += ' <img class="color-image" width="20px" src="./img/blueMana.png">';
+            }
+            if(mainColors.green > 0) {
+                html += ' <img class="color-image" width="20px" src="./img/greenMana.png">';
+            }
+            if(mainColors.red > 0) {
+                html += ' <img class="color-image" width="20px" src="./img/redMana.png">';
+            }
+            if(mainColors.white > 0) {
+                html += ' <img class="color-image" width="20px" src="./img/whiteMana.png">';
+            }
+        
+        
+              html += ' </li>'+
+                '</ul>'+
+           ' </div>'+
+        '</div>'+
+    '</div>';
     }
+    
+    
     
     $("#decks").empty().html(html);
 }
